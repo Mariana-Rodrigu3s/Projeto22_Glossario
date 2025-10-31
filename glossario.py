f2aa143aa9de9be33e4199f19b0591837e801993
Mariana Rodrigues <mariana.s.rodrigues13@aluno.senai.br>
2025-10-31 11:20:32 -0300

    Bom, eu adicionei o treeview, conectei com banco de dados, coloquei botao de adicionar na treeview e no banco de dados, fiz o excluir da treeview mas ainda não consegui excluir do banco de dados. Ainda falta fazer o atualizar

--- glossario.py
+++ glossario.py
@@ -1,64 +1,182 @@
 import ttkbootstrap as ttk
 from tkinter import messagebox
+from tkinter import Listbox
 import sqlite3
 
+class Glossario:
+    def __init__(self):
+        
 
 
-janela = ttk.Window(themename="minty",
-                                 title="Glossário de Termos Técnicos")
-janela.geometry("720x700")
-janela.resizable(False, False)
-
-tw = ttk.Treeview(janela)
-tw.pack(side="bottom", fill="both")
-
-tw["columns"] = ("termo", "definição", "categoria")
-
-tw["show"] = "headings"
-tw.heading("termo", text="Termo")
-tw.heading("definição", text="Definição")
-tw.heading("categoria", text="Categoria")
+    
+        # Janela Principal
+        self.janela = ttk.Window(themename="minty",
+                                        title="Glossário de Termos Técnicos")
+        self.janela.geometry("720x700")
+        self.janela.resizable(False, False)
 
 
-termo = ttk.Label(janela,
-          text="Digite o Termo:",
-          font=('Arial', 13)).place(x=20, y=27)
-entrada_termo = ttk.Entry(janela,
-                        width=110).place(x=20, y=54)
+        # Banco de Dados e Criação da Tabela
+        conexao = sqlite3.connect("./bddados.sqlite")
 
-definicao = ttk.Label(janela,
-                      text="Digite a Definição:",
-                      font=('Arial', 13)).place(x=20, y=150)
-entrada_definicao = ttk.Entry(janela,
-                              width=110).place(x=20, y=180)
+        cursor = conexao.cursor()
 
-categoria = ttk.Label(janela, 
-                      text="Digite a Categoria:",
-                      font=('Arial', 13)).place(x=20, y=270 )
+        glossario = """
+                        CREATE TABLE IF NOT EXISTS glossarios(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        termo VARCHAR(200),
+                        definição VARCHAR(5000),
+                        categoria VARCHAR(100));
+        """
 
-entrada_categoria = ttk.Entry(janela,
-                              width=110).place(x=20, y=298)
+        cursor.execute(glossario)
+        conexao.commit()
 
-botoes = ttk.Frame(janela).pack(pady=120)
+        cursor.close()
+        conexao.close()
 
 
+        # Labels
+        self.titulo = ttk.Label(self.janela,
+                        text="Glossário",
+                        font=('Ink Free', 25)).pack()
+        self.titulo2 = ttk.Label(self.janela,
+                            text="Termos Técnicos",
+                            font=('Ink Free', 25)).pack()
 
-adicionar = ttk.Button(botoes,
-                       style="outline button",
-                       text="Adicionar").pack(side="left", padx=10)
+        self.etiqueta = ttk.Label(self.janela,
+                            text="Crie seu próprio glossário de termos técnicos.").pack()
 
-excluir = ttk.Button(botoes,
-                     style="outline button",
-                     text="Excluir").pack(side="left", padx=10)
+        self.termo = ttk.Label(self.janela,
+                text="Digite o Termo:",
+                font=('Arial', 13)).place(x=20, y=100)
+        self.categoria = ttk.Label(self.janela, 
+                            text="Digite a Categoria:",
+                            font=('Arial', 13)).place(x=20, y=319 )
 
-alterar = ttk.Button(botoes,
-                     style="outline button",
-                     text="Alterar").pack(side="left", padx=10)
+        self.definicao = ttk.Label(self.janela,
+                            text="Digite a Definição:",
+                            font=('Arial', 13)).place(x=20, y=210)
+        
 
-conexao = sqlite3.connect("./bddados.sqlite")
+        # Campos de Entrada
+        self.entrada_termo = ttk.Entry(self.janela,
+                                width=110)
+        self.entrada_termo.place(x=20, y=130)
+
+        self.entrada_definicao = ttk.Entry(self.janela,
+                                    width=110)
+        self.entrada_definicao.place(x=20, y=240)
+        
+        self.entrada_categoria = ttk.Entry(self.janela,
+                                    width=110)
+        self.entrada_categoria.place(x=20, y=350)
+        
+
+        # TreeView (Tabela Visual)
+        self.tw = ttk.Treeview(self.janela)
+        self.tw.pack(side="bottom", fill="both")
+
+        self.tw["columns"] = ("id","termo", "definição", "categoria")
+
+        self.tw["show"] = "headings"
+        self.tw.heading("id", text="ID")
+        self.tw.heading("termo", text="Termo")
+        self.tw.heading("definição", text="Definição")
+        self.tw.heading("categoria", text="Categoria")
+        
+        self.tw.column("id", width=50)
+        self.tw.column("termo", width=50)
+        self.tw.column("definição", width=90)
+        self.tw.column("categoria", anchor="center")
+
+
+        # Botões
+        botoes = ttk.Frame(self.janela).pack(pady=130)
+        
+        self.adicionar = ttk.Button(botoes,
+                            style="outline button",
+                            text="Adicionar",
+                            width=30,
+                            command=self.adicionar_item).pack(padx=20, pady=10, side="left")
+
+        self.excluir = ttk.Button(botoes,
+                            style="outline button",
+                            text="Excluir",
+                            width=30,
+                            command=self.excluir_itens).pack(padx=20, pady=10, side="left")
+
+        self.alterar = ttk.Button(botoes,
+                            style="outline button",
+                            text="Alterar",
+                            width=30).pack(padx=20, pady=10, side="left")
+
 
 
         
+    # Adicionar Itens
+    def adicionar_item(self):
+        termos = self.entrada_termo.get()
+        definicoes = self.entrada_definicao.get()
+        categorias = self.entrada_categoria.get()
 
-janela.mainloop()
+        conexao = sqlite3.connect("./bddados.sqlite")
+        cursor = conexao.cursor()
+
+       
+
+        cursor.execute("""
+                INSERT INTO glossarios
+                
+                (termo,
+                definição,
+                categoria)
+                VALUES
+                (?,
+                ?,
+                ?)
+                    """,
+                    [termos,
+                     definicoes,
+                     categorias] )
+
+        conexao.commit()
+
+
+        conexao.close()
+
+        self.carregar_treeview()
+
+    def carregar_treeview(self):
+
+        conexao = sqlite3.connect("./bddados.sqlite")
+        cursor = conexao.cursor()
+
+        cursor.execute("SELECT * FROM glossarios")
+        dados = cursor.fetchall()
+
+        for linha in dados:
+            self.tw.insert("", "end", values=linha)
+
+        conexao.close()
+
+
+    def excluir_itens(self):
+        selecionado = self.tw.selection()
+        self.tw.delete(selecionado)
+
+  
+
+
+
+        
+                
+
+    def run(self):
+        self.janela.mainloop()
+
+
+if __name__ == "__main__":
+    gloss = Glossario()
+    gloss.janela.mainloop()
 
